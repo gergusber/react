11af5d3b4d1a08685bfbe60b78503b09df0a17cd
German Bertea <German.Bertea@travelport.com>
2021-10-05 17:39:28 +0200

    Simple css on jsx

--- Seccion3/react-complete-guide/src/components/ExpenseItem.js
+++ Seccion3/react-complete-guide/src/components/ExpenseItem.js
@@ -1,6 +1,14 @@
-
-const ExpenseItem = ()=>{
-return <h2>Expense Item!</h2>;
-}
+import './ExpenseItem.css'
+const ExpenseItem = () => {
+  return (
+    <div className="expense-item">
+      <div>Date</div>
+      <div className="expense-item__description">
+        <h2> Car Insurance</h2>
+        <div className="expense-item__price">1231</div>
+      </div>
+    </div>
+  );
+};
 
 export default ExpenseItem;
