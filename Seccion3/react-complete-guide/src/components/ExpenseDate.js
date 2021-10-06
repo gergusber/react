d0035e5c529a920c69ce62863ff2081cce2363ba
German Bertea <German.Bertea@travelport.com>
2021-10-06 13:19:47 +0200

    Create another component to mainteinance

---
+++ Seccion3/react-complete-guide/src/components/ExpenseDate.js
@@ -0,0 +1,14 @@
+const ExpenseDate = (props) => {
+  const month = props.date.toLocaleString("en-US", { month: "long" });
+  const day = props.date.toLocaleString("en-US", { month: "2-digit" });
+  const year = props.date.getFullYear();
+  return (
+    <div>
+      <div>{month}</div>
+      <div>{day}</div>
+      <div>{year}</div>
+    </div>
+  );
+};
+
+export default ExpenseDate;
--- Seccion3/react-complete-guide/src/components/ExpenseItem.js
+++ Seccion3/react-complete-guide/src/components/ExpenseItem.js
@@ -1,17 +1,10 @@
 import './ExpenseItem.css'
-import PropTypes from 'prop-types';
+import ExpenseDate from './ExpenseDate';
 const ExpenseItem = (props) => {
-const month = props.date.toLocaleString('en-US',{month:'long'});
-const day = props.date.toLocaleString('en-US',{month:'2-digit'});
-const year = props.date.getFullYear();
+
   return (
     <div className="expense-item">
-      <div>
-        <div>{month}</div>
-        <div>{day}</div>
-        <div>{year}</div>
-      </div>
-
+      <ExpenseDate date={new Date()} />
       <div className="expense-item__description">
         <h2> {props.title}</h2>
         <div className="expense-item__price">{props.amount}</div>
@@ -20,11 +13,5 @@ const year = props.date.getFullYear();
   );
 };
 
-// ExpenseItem.propTypes = {
-//   title: PropTypes.string,
-//   id: PropTypes.string,
-//   amount: PropTypes.string,
-//   date: PropTypes.string,
-// };
 
 export default ExpenseItem;
