76a88d8839ad1e0e4fdac54200966d083d1b0128
German Bertea <German.Bertea@travelport.com>
2021-10-06 09:36:22 +0200

    Addd logic for dates

--- Seccion3/react-complete-guide/src/components/ExpenseItem.js
+++ Seccion3/react-complete-guide/src/components/ExpenseItem.js
@@ -1,9 +1,17 @@
 import './ExpenseItem.css'
+import PropTypes from 'prop-types';
 const ExpenseItem = (props) => {
-
+const month = props.date.toLocaleString('en-US',{month:'long'});
+const day = props.date.toLocaleString('en-US',{month:'2-digit'});
+const year = props.date.getFullYear();
   return (
     <div className="expense-item">
-      <div>{props.date.toISOString()}</div>
+      <div>
+        <div>{month}</div>
+        <div>{day}</div>
+        <div>{year}</div>
+      </div>
+
       <div className="expense-item__description">
         <h2> {props.title}</h2>
         <div className="expense-item__price">{props.amount}</div>
@@ -11,5 +19,12 @@ const ExpenseItem = (props) => {
     </div>
   );
 };
+
+// ExpenseItem.propTypes = {
+//   title: PropTypes.string,
+//   id: PropTypes.string,
+//   amount: PropTypes.string,
+//   date: PropTypes.string,
+// };
 
 export default ExpenseItem;
