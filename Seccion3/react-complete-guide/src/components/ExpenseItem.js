f26b3a42f4c7ebc39649841b8e891ddf7fca8ead
German Bertea <German.Bertea@travelport.com>
2021-10-06 09:26:07 +0200

    Add props to a component

--- Seccion3/react-complete-guide/src/components/ExpenseItem.js
+++ Seccion3/react-complete-guide/src/components/ExpenseItem.js
@@ -1,11 +1,12 @@
 import './ExpenseItem.css'
-const ExpenseItem = () => {
+const ExpenseItem = (props) => {
+
   return (
     <div className="expense-item">
-      <div>Date</div>
+      <div>{props.date.toISOString()}</div>
       <div className="expense-item__description">
-        <h2> Car Insurance</h2>
-        <div className="expense-item__price">1231</div>
+        <h2> {props.title}</h2>
+        <div className="expense-item__price">{props.amount}</div>
       </div>
     </div>
   );
