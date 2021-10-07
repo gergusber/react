bb38b4d66cc62c4458633ccba5b16370ace298a9
German Bertea <German.Bertea@travelport.com>
2021-10-07 11:31:40 +0200

    Types of functions from onClick

--- Seccion3/react-complete-guide/src/components/Expenses/ExpenseItem.js
+++ Seccion3/react-complete-guide/src/components/Expenses/ExpenseItem.js
@@ -2,7 +2,9 @@ import './ExpenseItem.css'
 import Card from '../card/card'
 import ExpenseDate from './ExpenseDate';
 const ExpenseItem = (props) => {
-
+  const clickHandler =()=>{
+    console.log("Clicked")
+    }
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
@@ -10,6 +12,9 @@ const ExpenseItem = (props) => {
         <h2> {props.title}</h2>
         <div className="expense-item__price">{props.amount}</div>
       </div>
+      {/* <button onClick={()=>{console.log('On Click')}}>Change</button> */}
+      <button onClick={clickHandler}>Change</button>
+      {/* <button onClick={()=>{ clickHandler()}}>Change</button> */}
     </Card>
   );
 };
