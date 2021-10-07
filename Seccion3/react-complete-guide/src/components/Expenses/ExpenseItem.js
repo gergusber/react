0ac943927a122aa5c203a1f439ad4349eb9e3cfc
German Bertea <German.Bertea@travelport.com>
2021-10-07 12:18:39 +0200

    Add functionality to update the state

--- Seccion3/react-complete-guide/src/components/Expenses/ExpenseItem.js
+++ Seccion3/react-complete-guide/src/components/Expenses/ExpenseItem.js
@@ -1,20 +1,31 @@
 import './ExpenseItem.css'
 import Card from '../card/card'
 import ExpenseDate from './ExpenseDate';
+import React, { useState } from 'react'
+
 const ExpenseItem = (props) => {
-  const clickHandler =()=>{
-    console.log("Clicked")
-    }
+  const [title, setTitle] = useState(props.title);
+
+  const clickHandler = () => {
+    console.log("Clicked");
+    setTitle('updated');
+  };
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2> {props.title}</h2>
+        <h2> {title}</h2>
         <div className="expense-item__price">{props.amount}</div>
       </div>
       {/* <button onClick={()=>{console.log('On Click')}}>Change</button> */}
-      <button onClick={clickHandler}>Change</button>
-      {/* <button onClick={()=>{ clickHandler()}}>Change</button> */}
+      {/* <button onClick={clickHandler}>Change</button> */}
+      <button
+        onClick={() => {
+          clickHandler();
+        }}
+      >
+        Change
+      </button>
     </Card>
   );
 };
