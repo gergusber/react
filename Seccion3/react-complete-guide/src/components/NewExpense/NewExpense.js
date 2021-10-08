eee454c50794a41231bccdc7baef0d5dc617db33
German Bertea <German.Bertea@travelport.com>
2021-10-08 08:43:25 +0200

    Add new Expense component

---
+++ Seccion3/react-complete-guide/src/components/NewExpense/NewExpense.js
@@ -0,0 +1,17 @@
+import React from 'react'
+// import PropTypes from 'prop-types'
+import './NewExpense.css'
+import ExpenseForm from './ExpenseForm'
+const NewExpense = () => {
+    return (
+        <div className="new-expense">
+           <ExpenseForm />
+        </div>
+    )
+}
+
+// NewExpense.propTypes = {
+
+// }
+
+export default NewExpense
