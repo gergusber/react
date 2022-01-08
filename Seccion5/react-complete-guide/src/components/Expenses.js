605904f1ebd3fe8800a616d4acdb1e68a9a6b362
German Bertea <gebe@influencerindex.com>
2022-01-08 17:58:34 +0100

    update project

---
+++ Seccion5/react-complete-guide/src/components/Expenses.js
@@ -0,0 +1,33 @@
+import Card from "./card/card";
+import "./Expenses.css";
+import ExpensesFilter from "./Filter/ExpensesFilter";
+import { useState } from "react";
+import ExpensesList from "./ExpensesList/ExpensesList";
+import ExpensesChart from "./Expenses/ExpensesChart/ExpensesChart";
+const Expenses = (props) => {
+  const [filterYear, setfilterYear] = useState("2020");
+
+  const Filter = (value) => {
+    setfilterYear(value);
+    console.log("FilterFrom state", value);
+  };
+
+  const filterExpenses = props.expenses.filter((x) => {
+    return x.date.getFullYear().toString() === filterYear;
+  });
+
+  return (
+    <li>
+      <Card className="expenses">
+        <div>
+          <ExpensesFilter onSelectNewFilter={Filter} selected={filterYear} />
+        </div>
+        <ExpensesChart expenses={filterExpenses} />
+
+        <ExpensesList items={filterExpenses} />
+      </Card>
+    </li>
+  );
+};
+
+export default Expenses;
---
+++ Seccion5/react-complete-guide/src/components/Filter/ExpensesFilter.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+import "./ExpensesFilter.css";
+
+const ExpensesFilter = (props) => {
+  const filterchangeHandler = (ev) => {
+    props.onSelectNewFilter(ev.target.value);
+  };
+
+  return (
+    <div className="expenses-filter">
+      <div className="expenses-filter__control">
+        <label>Filter by year</label>
+        <select onChange={filterchangeHandler} value={props.selected}>
+          <option value="2022">2022</option>
+          <option value="2021">2021</option>
+          <option value="2020">2020</option>
+          <option value="2019">2019</option>
+        </select>
+      </div>
+    </div>
+  );
+};
+
+export default ExpensesFilter;
