5a6fb189ade274873d694d5e0fc7cbf34585be31
German Bertea <gebe@influencerindex.com>
2022-03-07 20:47:56 +0100

    add styles with dinamic class

--- Seccion6/src/components/CourseGoals/CourseInput/CourseInput.js
+++ Seccion6/src/components/CourseGoals/CourseInput/CourseInput.js
@@ -8,6 +8,10 @@ const CourseInput = props => {
   const [isValid, setIsValid] = useState(true)
 
   const goalInputChangeHandler = event => {
+    if(event.target.value.trim().length > 0){
+      setIsValid(true);
+    }
+
     setEnteredValue(event.target.value);
   };
 
@@ -23,11 +27,9 @@ const CourseInput = props => {
 
   return (
     <form onSubmit={formSubmitHandler}>
-      <div className="form-control">
-        <label style={{ color: !isValid ? 'red' : 'black' }}>Course Goal</label>
-        <input style={{ borderColor: !isValid ? 'red' : 'black', 
-                  backgroundColor: !isValid ? 'salmon' : 'transparent' }} 
-        type="text" onChange={goalInputChangeHandler} />
+      <div className={ `form-control ${!isValid ? 'invalid':''}`}>
+        <label >Course Goal</label>
+        <input type="text" onChange={goalInputChangeHandler} />
       </div>
       <Button type="submit">Add Goal</Button>
     </form>
