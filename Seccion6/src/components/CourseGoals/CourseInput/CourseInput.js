e128442417ffa5cd6f077bd9d6bed2a00560ea7e
German Bertea <gebe@influencerindex.com>
2022-08-03 19:36:45 +0200

    dinamyc class with data

--- Seccion6/src/components/CourseGoals/CourseInput/CourseInput.js
+++ Seccion6/src/components/CourseGoals/CourseInput/CourseInput.js
@@ -1,33 +1,6 @@
 import React, { useState } from 'react';
 import Button from '../../UI/Button/Button';
-import './CourseInput.css';
-import styled from 'styled-components';
-
-const FromControl = styled.div`
-  margin: 0.5rem 0;
- $ label {
-  font-weight: bold;
-  display: block;
-  margin-bottom: 0.5rem;
-  color:  ${props => (props.invalid ? 'red' : 'black')}
-}
-
-$ input {
-  display: block;
-  width: 100%;
-  border: 1px solid ${props => (props.invalid ? 'red' : '#ccc')}
-  background: ${props => (props.invalid ? '#fad0ec' : 'transparent')}
-  font: inherit;
-  line-height: 1.5rem;
-  padding: 0 0.25rem;
-}
-
-$ input:focus {
-  outline: none;
-  background: #fad0ec;
-  border-color: #8b005d;
-}
-`;
+import styles from './CourseInput.module.css';
 
 const CourseInput = props => {
   const [enteredValue, setEnteredValue] = useState('');
@@ -53,10 +26,10 @@ const CourseInput = props => {
 
   return (
     <form onSubmit={formSubmitHandler}>
-      <FromControl invalid={!isValid}>
+      <div className={`${styles['form-control']} ${!isValid && styles.invalid}`}>
         <label >Course Goal</label>
         <input type="text" onChange={goalInputChangeHandler} />
-      </FromControl>
+      </div>
       <Button type="submit">Add Goal</Button>
     </form>
   );
