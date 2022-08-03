49a3b2c3b4083b8cfbdffed6ae2508d046f76952
German Bertea <gebe@influencerindex.com>
2022-08-03 19:22:23 +0200

    add media queries

--- Seccion6/src/components/UI/Button/Button.js
+++ Seccion6/src/components/UI/Button/Button.js
@@ -12,6 +12,7 @@ import styled from 'styled-components'
 
 
 const Button = styled.button`
+  width: 100%;
   font: inherit;
   padding: 0.5rem 1.5rem;
   border: 1px solid #8b005d;
@@ -20,16 +21,20 @@ const Button = styled.button`
   box-shadow: 0 0 4px rgba(0, 0, 0, 0.26);
   cursor: pointer;
 
- &:focus{ 
-  outline: none;
-}
+  @media (min-width: 768) {
+    width: auto;
+  }
 
-&:hover,
-&:active {
-  background: #ac0e77;
-  border-color: #ac0e77;
-  box-shadow: 0 0 8px rgba(0, 0, 0, 0.26);
-}
+  &:focus{ 
+    outline: none;
+  }
+
+  &:hover,
+  &:active {
+    background: #ac0e77;
+    border-color: #ac0e77;
+    box-shadow: 0 0 8px rgba(0, 0, 0, 0.26);
+  }
 `;
 
 
