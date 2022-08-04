ff86c0c2f4789e9d373d06abd3a9d54ba7a18be1
German Bertea <gebe@influencerindex.com>
2022-08-04 18:44:03 +0200

    add first draft of ccomponents

--- Seccion8/src/App.js
+++ Seccion8/src/App.js
@@ -1,10 +1,32 @@
-import React from 'react';
-
+import React, { useState } from 'react';
+import { AddUser } from "../src/components/Users/userAdd/AddUser";
 
 function App() {
+  const [usersList, setUsersList] = useState([
+    { username: 'gertea', age: 30 },
+    { username: 'gertea2', age: 30 },
+  ]);
+
+  let content = (
+    <p style={{ textAlign: 'center' }}>No users found. Maybe add one?</p>
+  );
+
+  if (usersList.length > 0) {
+    content = (
+      <p> hola </p>
+      // <CourseGoalList items={usersList} onDeleteItem={deleteItemHandler} />
+    );
+  }
+
+
   return (
     <div>
-
+      <section id="goal-form">
+        <AddUser />
+      </section>
+      <section id="goals">
+        {content}
+      </section>
     </div>
   );
 }
---
+++ Seccion8/src/components/Users/userAdd/AddUser.js
@@ -0,0 +1,40 @@
+import React, { useState } from 'react'
+import styles from './AddUser.module.css';
+import Button from '../../UI/Button/Button';
+import Card from '../../UI/Card/Card';
+
+export const AddUser = (props) => {
+    const [userData, setUserData] = useState({ username: '', age: 0 });
+    const [isValid, setIsValid] = useState(true)
+
+    const usernameInputChangeHandler = event => {
+        if (event.target.value.trim().length > 0) {
+            setIsValid(true);
+        }
+
+        setUserData(event.target.value);
+    };
+
+
+    const onAddUser = (event) => {
+        event.preventDefault();
+        props.onAddGoal(userData);
+    }
+
+    return (
+        <Card>
+            <form onSubmit={onAddUser}>
+                <div className={styles.input}>
+                    <label htmlFor='username'> UserName</label>
+                    <input type="text" id="username" />
+
+                    <label htmlFor='age'> Age</label>
+                    <input type="text" id="age" />
+                </div>
+                
+                <Button type="submit">Add User</Button>
+            </form>
+        </Card>
+
+    )
+}
