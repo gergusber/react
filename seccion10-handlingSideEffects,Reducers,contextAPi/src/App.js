3e985a5d9ca58f6784ed2ad55655711c0c366697
German Bertea <gebe@influencerindex.com>
2022-08-05 18:16:02 +0200

    update login

--- seccion10-handlingSideEffects,Reducers,contextAPi/src/App.js
+++ seccion10-handlingSideEffects,Reducers,contextAPi/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import Login from './components/Login/Login';
 import Home from './components/Home/Home';
@@ -7,16 +7,27 @@ import MainHeader from './components/MainHeader/MainHeader';
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
+  useEffect(() => {
+    const storedUSerLoggedIn = localStorage.getItem('isLoggedIn');
+    if(storedUSerLoggedIn === '1'){
+      setIsLoggedIn(true);
+    }
+  }, [])
+
+
   const loginHandler = (email, password) => {
     // We should of course check email and password
     // But it's just a dummy/ demo anyways
+    localStorage.setItem('isLoggedIn', '1')
     setIsLoggedIn(true);
   };
 
+
   const logoutHandler = () => {
+    localStorage.removeItem('isLoggedIn');
     setIsLoggedIn(false);
   };
-
+  
   return (
     <React.Fragment>
       <MainHeader isAuthenticated={isLoggedIn} onLogout={logoutHandler} />
