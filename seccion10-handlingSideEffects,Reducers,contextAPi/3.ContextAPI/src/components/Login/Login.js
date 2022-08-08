0b7e4821ab7b59e6ff60190e10d0855a936d522a
German Bertea <gebe@influencerindex.com>
2022-08-08 12:24:14 +0200

    add context and using a state function  auth provider

--- seccion10-handlingSideEffects,Reducers,contextAPi/3.ContextAPI/src/components/Login/Login.js
+++ seccion10-handlingSideEffects,Reducers,contextAPi/3.ContextAPI/src/components/Login/Login.js
@@ -1,8 +1,9 @@
-import React, { useState, useEffect, useReducer } from 'react';
+import React, { useState, useEffect, useReducer,useContext } from 'react';
 
 import Card from '../UI/Card/Card';
 import classes from './Login.module.css';
 import Button from '../UI/Button/Button';
+import AuthContext from '../../store/auth-context';
 
 const emailReducer = (state, action) => {
   if (action.type === 'USER_INPUT') {
@@ -28,6 +29,10 @@ const Login = (props) => {
   const [formIsValid, setFormIsValid] = useState(false);
   const [emailState, dispatchEmail] = useReducer(emailReducer, { value: '', isValid: null })
   const [passwordState, dispatchPassword] = useReducer(passwordReducer, { value: '', isValid: null })
+
+  
+  const ctx = useContext(AuthContext)
+
 
   useEffect(() => {
     console.log('EFFECT RUNNING');
@@ -76,7 +81,7 @@ const Login = (props) => {
 
   const submitHandler = (event) => {
     event.preventDefault();
-    props.onLogin(emailState.value, passwordState.value);
+    ctx.onLogin(emailState.value, passwordState.value);
   };
 
   return (
