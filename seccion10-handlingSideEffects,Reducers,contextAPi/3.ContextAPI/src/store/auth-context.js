884635719aec5ef090d7cedb30edb8cb2099a2ab
German Bertea <gebe@influencerindex.com>
2022-08-08 12:08:32 +0200

    initialize context

--- seccion10-handlingSideEffects,Reducers,contextAPi/3.ContextAPI/src/store/auth-context.js
+++ seccion10-handlingSideEffects,Reducers,contextAPi/3.ContextAPI/src/store/auth-context.js
@@ -2,7 +2,8 @@ import React from 'react'
 
 
 const AuthContext = React.createContext({
-    isLoggedIn: false
+    isLoggedIn: false,
+    onLogout: () => { } //initiealize context
 });
 
 export default AuthContext;
