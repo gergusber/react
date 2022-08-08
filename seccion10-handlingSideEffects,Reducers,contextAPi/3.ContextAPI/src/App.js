861d0d59b13b59e4ec38caf994b67e349120cc50
German Bertea <gebe@influencerindex.com>
2022-08-08 12:06:07 +0200

    use context to forward handlers

--- seccion10-handlingSideEffects,Reducers,contextAPi/3.ContextAPI/src/App.js
+++ seccion10-handlingSideEffects,Reducers,contextAPi/3.ContextAPI/src/App.js
@@ -28,13 +28,18 @@ function App() {
   };
 
   return (
-    <AuthContext.Provider value={{ isLoggedIn: isLoggedIn }}>
-        <MainHeader onLogout={logoutHandler} />
-        <main>
-          {!isLoggedIn && <Login onLogin={loginHandler} />}
-          {isLoggedIn && <Home onLogout={logoutHandler} />}
-        </main>
-      </AuthContext.Provider >
+    <AuthContext.Provider value={
+      {
+        isLoggedIn: isLoggedIn,
+        onLogout: logoutHandler,
+      }
+    }>
+      <MainHeader />
+      <main>
+        {!isLoggedIn && <Login onLogin={loginHandler} />}
+        {isLoggedIn && <Home onLogout={logoutHandler} />}
+      </main>
+    </AuthContext.Provider >
   );
 }
 
