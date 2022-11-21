07961d4a16a67e4505db22bc601ddc93891f1435
German Bertea <gebe@influencerindex.com>
2022-11-21 20:59:01 +0100

    add card data

--- seccion11-PracticeProjectBuildingaFoodOrderApp/src/components/cart/Cart/Cart.js
+++ seccion11-PracticeProjectBuildingaFoodOrderApp/src/components/cart/Cart/Cart.js
@@ -1,25 +1,47 @@
 import classes from './Cart.module.css'
-import Modal  from '../../UI/modal/Modal'
+import Modal from '../../UI/modal/Modal'
+import React, { useContext } from 'react'
+import CartContext from '../../../store/cart-context'
+import CartItem from './components/CartItem'
 
-const items = [{id: 'c1', name: 'sushi', amount: 2, price: 12},
-  {id: 'c2', name: 'sushi2', amount: 2, price: 12},
-  {id: 'c3', name: 'sushi3', amount: 2, price: 12}]
+
 
 const Cart = props => {
+
+  const cartCtx = useContext(CartContext)
+
+  const totalAmount = `$80`;
+
+  const hasItems = cartCtx.items.length > 0;
+
+  const cartItemRemoveHandler = props => {
+
+  }
+  const cartItemAddHandler = props => {
+
+  }
   const cartItems = (
     <ul className={classes.cartItems}>
-      {items.map((item) => <li>{item.name}</li>)}
-  </ul>)
+      {cartCtx.items.map((item) =>
+        <CartItem key={item.id}
+          name={item.name}
+          amount={item.amount}
+          price={item.price}
+          onRemove={cartItemRemoveHandler.bind(null,item.id)}
+          onAdd={cartItemAddHandler.bind(null,item)}
+        />)}
+    </ul>)
+
   return (
     <Modal onClose={props.onHideCart}>
       {cartItems}
       <div className={classes.total}>
         <span> total amount: </span>
-        <span> 35.60</span>
+        <span> {totalAmount} </span>
       </div>
       <div className={classes.actions} >
         <button className={classes.buttonAlt} onClick={props.onHideCart}> close</button>
-        <button className={classes.button}>order</button>
+        {hasItems && <button className={classes.button}>order</button>}
       </div>
     </Modal>
   )
