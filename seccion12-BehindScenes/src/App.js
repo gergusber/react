e06b2b37444b402bbf7910babde9f9104695ff83
German Bertea <gebe@influencerindex.com>
2022-11-28 23:26:13 +0100

    add toggle btn

--- seccion12-BehindScenes/src/App.js
+++ seccion12-BehindScenes/src/App.js
@@ -1,11 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import './App.css';
+import Button from './components/UI/Button/Button'
 
 function App() {
+  const [showParagraph, setShowParagraph] = useState(false)
+  const toggleP = () => {
+    setShowParagraph(showParagraph => !showParagraph)
+  }
   return (
     <div className="app">
       <h1>Hi there!</h1>
+      {showParagraph && <p> holaa</p>}
+      <Button onClick={toggleP} > toggle paragraph</Button>
     </div>
   );
 }
