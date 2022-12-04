3d1577146656aeb0992ac8f8ddb22649895ddeb4
German Bertea <gebe@influencerindex.com>
2022-12-04 19:56:23 +0100

    update app for new steps

--- seccion14-SendingHttpRequests/src/App.js
+++ seccion14-SendingHttpRequests/src/App.js
@@ -1,61 +1,71 @@
 import React, { useState, useEffect, useCallback } from 'react';
 
 import MoviesList from './components/MoviesList';
+import AddMovie from './components/AddMovie';
 import './App.css';
 
 function App() {
   const [movies, setMovies] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState(null)
-  const fetchMoviesHandler = useCallback(async() => {
-    
-    setIsLoading(true)
-    setError(null)
+  const [error, setError] = useState(null);
+
+  const fetchMoviesHandler = useCallback(async () => {
+    setIsLoading(true);
+    setError(null);
     try {
-      const response = await fetch('https://swapi.py4e.com/api/films');
+      const response = await fetch('https://swapi.dev/api/films/');
       if (!response.ok) {
-        throw new Error('something went wrong')
+        throw new Error('Something went wrong!');
       }
-      const data = await response.json()
-      const transformedMovies = data.results.map(x => {
+
+      const data = await response.json();
+
+      const transformedMovies = data.results.map((movieData) => {
         return {
-          id: x.episode_id,
-          title: x.title,
-          openingText: x.opening_crawl,
-          releaseDate: x.release_date
-        }
-      })
-      setMovies(transformedMovies)
+          id: movieData.episode_id,
+          title: movieData.title,
+          openingText: movieData.opening_crawl,
+          releaseDate: movieData.release_date,
+        };
+      });
+      setMovies(transformedMovies);
     } catch (error) {
-      setError(error.message)
+      setError(error.message);
     }
-    setIsLoading(false)
-  }, [])
+    setIsLoading(false);
+  }, []);
 
   useEffect(() => {
-    fetchMoviesHandler()
-  }, [fetchMoviesHandler])
+    fetchMoviesHandler();
+  }, [fetchMoviesHandler]);
 
-  let content = <p>Found not movies.</p>
-  if (movies.length > 0) {
-    content = <MoviesList movies={movies} />
+  function addMovieHandler(movie) {
+    console.log(movie);
   }
+
+  let content = <p>Found no movies.</p>;
+
+  if (movies.length > 0) {
+    content = <MoviesList movies={movies} />;
+  }
+
   if (error) {
-    content = <p>{error}</p>
+    content = <p>{error}</p>;
   }
 
   if (isLoading) {
-    content = <p>Loading...</p>
+    content = <p>Loading...</p>;
   }
 
   return (
     <React.Fragment>
       <section>
-        <button onClick={fetchMoviesHandler}>Fetch Movies</button>
+        <AddMovie onAddMovie={addMovieHandler} />
       </section>
       <section>
-        {content}
+        <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
+      <section>{content}</section>
     </React.Fragment>
   );
 }
