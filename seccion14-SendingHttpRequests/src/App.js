26366be2d902c38dfe69586acdf3ecdac3a604ac
German Bertea <gebe@influencerindex.com>
2022-12-04 19:44:44 +0100

    add error message if error on url or in api

--- seccion14-SendingHttpRequests/src/App.js
+++ seccion14-SendingHttpRequests/src/App.js
@@ -4,23 +4,34 @@ import MoviesList from './components/MoviesList';
 import './App.css';
 
 function App() {
-  const [movies, setMovies] = useState([])
-  const [isLoading, setIsLoading] = useState(false)
-
+  const [movies, setMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null)
   const fetchMoviesHandler = async () => {
     setIsLoading(true)
-    const response = await fetch('https://swapi.py4e.com/api/films');
-    const data = await response.json()
-    const transformedMovies = data.results.map(x => {
-      return {
-        id: x.episode_id,
-        title: x.title,
-        openingText: x.opening_crawl,
-        releaseDate: x.release_date
+    setError(null)
+
+    try {
+      const response = await fetch('https://swapi.py4e.com/api/films');
+      if (!response.ok) {
+        throw new Error('something went wrong')
       }
-    })
-    setMovies(transformedMovies)
+
+      const data = await response.json()
+      const transformedMovies = data.results.map(x => {
+        return {
+          id: x.episode_id,
+          title: x.title,
+          openingText: x.opening_crawl,
+          releaseDate: x.release_date
+        }
+      })
+      setMovies(transformedMovies)
+    } catch (error) {
+      setError(error.message)
+    }
     setIsLoading(false)
+
   }
 
   // useEffect(() => {
@@ -35,8 +46,10 @@ function App() {
       </section>
       <section>
         {!isLoading && <MoviesList movies={movies} />}
-        {!isLoading && movies.length === 0 && <p>Found not movies.</p>}
+        {!isLoading && movies.length === 0 && !error && <p>Found not movies.</p>}
         {isLoading && <p>Loading...</p>}
+        {!isLoading && error && <p>{error}</p>}
+
       </section>
     </React.Fragment>
   );
