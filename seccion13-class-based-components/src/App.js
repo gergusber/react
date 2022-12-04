7df8f91782001e1d64edab1ef33bda1e23ff1a01
German Bertea <gebe@influencerindex.com>
2022-12-04 18:11:56 +0100

    add  store

--- seccion13-class-based-components/src/App.js
+++ seccion13-class-based-components/src/App.js
@@ -1,11 +1,23 @@
-import Users from './components/Users';
+
+import UserFinder from './components/UserFinder';
+import UsersContext from './store/users-context';
+const DUMMY_USERS = [
+  { id: 'u1', name: 'Max' },
+  { id: 'u2', name: 'Manuel' },
+  { id: 'u3', name: 'Julie' },
+];
 
 function App() {
+  const usersContext = {
+    users: DUMMY_USERS
+  }
+
   return (
-    <div>
-      <Users />
-    </div>
+    <UsersContext.Provider value={usersContext}>
+      <UserFinder />
+    </UsersContext.Provider>
   );
 }
+
 
 export default App;
---
+++ seccion13-class-based-components/src/store/users-context.js
@@ -0,0 +1,8 @@
+import { createContext } from "react";
+
+const UserContext = createContext({
+  users: []
+})
+
+
+export default UserContext 
