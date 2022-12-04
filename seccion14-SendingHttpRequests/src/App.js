f50f66e0bfa2e3004ed8e89f2c1b8ac4f8e08557
German Bertea <gebe@influencerindex.com>
2022-12-04 19:30:16 +0100

    async await version

--- seccion14-SendingHttpRequests/src/App.js
+++ seccion14-SendingHttpRequests/src/App.js
@@ -5,22 +5,36 @@ import './App.css';
 
 function App() {
   const [movies, setMovies] = useState([])
-  const fetchMoviesHandler = () => {
-    fetch('https://swapi.py4e.com/api/films')
-      .then(response => { return response.json() })
-      .then(
-        (res) => {
-          const data = res.results.map(x => {
-            return {
-              id: x.episode_id,
-              title: x.title,
-              openingText: x.opening_crawl,
-              releaseDate: x.release_date
-            }
-          })
-          setMovies(data)
-        }
-      )
+  // const fetchMoviesHandler = () => {
+  //   fetch('https://swapi.py4e.com/api/films')
+  //     .then(response => { return response.json() })
+  //     .then(
+  //       (res) => {
+  //         const data = res.results.map(x => {
+  //           return {
+  //             id: x.episode_id,
+  //             title: x.title,
+  //             openingText: x.opening_crawl,
+  //             releaseDate: x.release_date
+  //           }
+  //         })
+  //         setMovies(data)
+  //       }
+  //     )
+  // }
+  // async await version
+  const fetchMoviesHandler = async () => {
+    const response = await fetch('https://swapi.py4e.com/api/films');
+    const data = await response.json()
+    const transformedMovies = data.results.map(x => {
+      return {
+        id: x.episode_id,
+        title: x.title,
+        openingText: x.opening_crawl,
+        releaseDate: x.release_date
+      }
+    })
+    setMovies(transformedMovies)
   }
 
   // useEffect(() => {
