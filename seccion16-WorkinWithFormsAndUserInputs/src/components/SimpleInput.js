64bf944bbba28f8d9191987f4cbe2caf0ea3f2e9
German Bertea <gebe@influencerindex.com>
2022-12-08 11:11:06 +0100

    update state with refs and state

--- seccion16-WorkinWithFormsAndUserInputs/src/components/SimpleInput.js
+++ seccion16-WorkinWithFormsAndUserInputs/src/components/SimpleInput.js
@@ -1,9 +1,26 @@
+import { useRef, useState } from "react";
+
 const SimpleInput = (props) => {
+  const [enteredName, setEnteredName] = useState('')
+  const nameInputRef = useRef()
+  const nameInputChangedHandler = event => {
+    setEnteredName(event.target.value)
+  }
+
+  const formSubmissionHandler = event => {
+    event.preventDefault();
+
+    console.log('enteredName state',enteredName)
+    console.log('nameInputRef',nameInputRef.current.value)
+    
+    setEnteredName('')
+  }
+
   return (
-    <form>
+    < form onSubmit={formSubmissionHandler}>
       <div className='form-control'>
         <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' />
+        <input type='text' id='name' ref={nameInputRef} onChange={nameInputChangedHandler} value={enteredName} />
       </div>
       <div className="form-actions">
         <button>Submit</button>
