bae365d5370cc528de85a0027f8c5343acae40af
German Bertea <gebe@influencerindex.com>
2022-12-08 11:18:33 +0100

    add css

--- seccion16-WorkinWithFormsAndUserInputs/src/components/SimpleInput.js
+++ seccion16-WorkinWithFormsAndUserInputs/src/components/SimpleInput.js
@@ -1,26 +1,40 @@
 import { useRef, useState } from "react";
 
 const SimpleInput = (props) => {
-  const [enteredName, setEnteredName] = useState('')
-  const nameInputRef = useRef()
+  const [enteredName, setEnteredName] = useState('');
+  const nameInputRef = useRef();
+  const [enteredNameValid, setEnteredNameValid] = useState(true)
+
   const nameInputChangedHandler = event => {
     setEnteredName(event.target.value)
   }
 
   const formSubmissionHandler = event => {
     event.preventDefault();
+    if (enteredName.trim() === '') {
+      setEnteredNameValid(false)
+      return
+    }
+    setEnteredNameValid(true)
 
-    console.log('enteredName state',enteredName)
-    console.log('nameInputRef',nameInputRef.current.value)
-    
+    console.log('enteredName state', enteredName)
+    console.log('nameInputRef', nameInputRef.current.value)
+
     setEnteredName('')
   }
+  //we assign classes for invalid css conditionally
+  const nameInputClasses = enteredNameValid ? 'form-control' : 'form-control invalid';
 
   return (
     < form onSubmit={formSubmissionHandler}>
-      <div className='form-control'>
+      <div className={nameInputClasses}>
         <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' ref={nameInputRef} onChange={nameInputChangedHandler} value={enteredName} />
+        <input type='text'
+          id='name'
+          ref={nameInputRef}
+          onChange={nameInputChangedHandler}
+          value={enteredName} />
+        {!enteredNameValid && <p className="error-text">Name must not be empty.</p>}
       </div>
       <div className="form-actions">
         <button>Submit</button>
