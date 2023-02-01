b49865ed85f733ddb5da181059ce4ef36a98d3b4
German Bertea <gebe@influencerindex.com>
2023-02-01 20:42:45 +0100

    add redux toolkit to the state

--- seccion18-DivingintoRedux/2.reduxAndReactDemo/src/components/Counter.js
+++ seccion18-DivingintoRedux/2.reduxAndReactDemo/src/components/Counter.js
@@ -1,5 +1,6 @@
 import classes from './Counter.module.css';
 import { useSelector, useDispatch } from 'react-redux'
+import { counterActions } from '../store/index'
 
 const Counter = () => {
   const dispatch = useDispatch();
@@ -7,18 +8,18 @@ const Counter = () => {
   const show = useSelector(state => state.showCounter)
 
   const increaseHandler = () => {
-    dispatch({ type: 'increase', amount: 5 })
+    dispatch(counterActions.increase(5))
   }
 
   const incrementHandler = () => {
-    dispatch({ type: 'increment' })
+    dispatch(counterActions.increment())
   }
 
   const decrementHandler = () => {
-    dispatch({ type: 'decrement' })
+    dispatch(counterActions.decrement())
   }
   const toggleCounterHandler = () => {
-    dispatch({ type: 'toggle' })
+    dispatch(counterActions.toggleCounter())
   };
 
   return (
--- seccion18-DivingintoRedux/2.reduxAndReactDemo/src/store/index.js
+++ seccion18-DivingintoRedux/2.reduxAndReactDemo/src/store/index.js
@@ -1,42 +1,48 @@
-const redux = require('redux');
+// const redux = require('redux');
+// import classes from './Counter.module.css';
+// import {createStore} from 'reud';
+// import { createStore } from 'redux'
+import { createSlice, configureStore } from '@reduxjs/toolkit';
+
 const actions = {
   INCREMENT: 'increment',
   DECREMENT: 'decrement',
   INCREASE: 'increase',
   TOGGLE: 'toggle'
 }
+
 const initialState = { counter: 0, showCounter: true }
-const counterReducer = (state = initialState, action) => {
-  if (action === actions.INCREASE) {
-    return {
-      counter: state.counter + action.amount,
-      showCounter: action.showCounter
+
+const counterSlicer = createSlice({
+  name: 'counter',
+  initialState,
+  reducers: {
+    increment(state) {// counterSlicer.actions.increase
+      state.counter++
+    },
+    decrement(state) {// counterSlicer.actions.decrement
+      state.counter--
+    },
+    increase(state, action) {// counterSlicer.actions.increment
+      state.counter = state.counter + action.payload
+    },
+    toggleCounter(state) {// counterSlicer.actions.toggleCounter
+      state.showCounter = !state.showCounter
     }
   }
-  if (action === actions.INCREMENT) {
-    return {
-      counter: state.counter + 1,
-      showCounter: action.showCounter
-    }
-  }
-  if (action === actions.DECREMENT) {
-    return {
-      counter: state.counter - 1,
-      showCounter: action.showCounter
-    }
-  }
-
-  if (action === actions.TOGGLE) {
-    return {
-      counter: state.counter,
-      showCounter: !action.showCounter
-    }
-  }
-
-  return state;
-}
-
-const store = redux.createStore(counterReducer);
+})
 
 
+
+
+const store = configureStore({
+  reducer: counterSlicer.reducer,
+  //for multiple reducers
+  // reducer:{
+  //   counter: counterSlicer.reducer
+  // }
+  
+});
+
+export const counterActions = counterSlicer.actions;
 export default store;
