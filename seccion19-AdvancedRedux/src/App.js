d6575803b31e391a8da7227a8c0d2704cb65ca36
German Bertea <gebe@influencerindex.com>
2023-02-04 17:19:10 +0100

    create thunk for dispatch actions

--- seccion19-AdvancedRedux/src/App.js
+++ seccion19-AdvancedRedux/src/App.js
@@ -3,7 +3,7 @@ import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
 import { useSelector, useDispatch } from 'react-redux'
 import { useEffect } from 'react'
-import { uiActions } from './store/slices/ui-slice'
+import { sendCartData } from './store/slices/cart-slice'
 import Notification from './components/Cart/Notification'
 
 let isInitial = true;
@@ -15,44 +15,11 @@ function App() {
   const notification = useSelector(state => state.ui.notification)
 
   useEffect(() => {
-    const sendCartData = async () => {
-      dispatch(uiActions.showNOtification({
-        status: 'pending',
-        title: 'sending...',
-        message: 'sending message'
-      }))
-
-      const response = await fetch('firebase url', //
-        {
-          method: 'PUT',
-          body: JSON.stringify(cart)
-        })
-
-      if (!response.ok) {
-        throw new Error('Send cart data failed')
-      }
-
-
-      dispatch(uiActions.showNOtification({
-        status: 'success',
-        title: 'Success',
-        message: 'sent cart data successfully'
-      }))
-    }
-
     if (isInitial) {
       isInitial = false
       return
     }
-
-    sendCartData().catch(() => {
-      dispatch(uiActions.showNOtification({
-        status: 'error',
-        title: 'Error',
-        message: 'sent cart data Error'
-      }))
-    })
-
+    dispatch(sendCartData(cart)) //create a thunk(cart)) that calls the action that we need and the fetch
   }, [cart, dispatch])
   return (
     <>
--- seccion19-AdvancedRedux/src/store/slices/cart-slice.js
+++ seccion19-AdvancedRedux/src/store/slices/cart-slice.js
@@ -1,5 +1,5 @@
 import { createSlice } from '@reduxjs/toolkit';
-
+import { uiActions } from "../slices/ui-slice";
 const initialStateCart = {
   items: [],
   totalQuantity: 0,
@@ -41,8 +41,47 @@ const cartSlice = createSlice({
         existingItem.quantity--;
       }
     },
+
   }
 })
+
+export const sendCartData = (cart) => {
+  return async (dispatch) => {
+    dispatch(uiActions.showNOtification({
+      status: 'pending',
+      title: 'sending...',
+      message: 'sending message'
+    }))
+
+    const sendRequest = async () => {
+      const response = await fetch('url from firebase', 
+        {
+          method: 'PUT',
+          body: JSON.stringify(cart)
+        })
+
+      if (!response.ok) {
+        throw new Error('Send cart data failed')
+      }
+    }
+
+    try {
+      await sendRequest();
+      dispatch(uiActions.showNOtification({
+        status: 'success',
+        title: 'Success',
+        message: 'sent cart data successfully'
+      }))
+
+    } catch (error) {
+      dispatch(uiActions.showNOtification({
+        status: 'error',
+        title: 'Error',
+        message: 'sent cart data Error'
+      }))
+    }
+  }
+}
 export const cartActions = cartSlice.actions;// export the actions of the slicer 
 
 export default cartSlice.reducer
