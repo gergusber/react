3d0463f31baf129d3335daeca979996ebfba5c23
German Bertea <gebe@influencerindex.com>
2023-02-06 20:36:43 +0100

    routing with objs

--- seccion20-ReactRouter/src/App.js
+++ seccion20-ReactRouter/src/App.js
@@ -1,24 +1,25 @@
 import {
   createBrowserRouter,
-  createRoutesFromElements,
-  Route,
+  // createRoutesFromElements,
+  // Route,
   RouterProvider
 } from 'react-router-dom'
 import Homepage from './pages/Home'
 import Products from './pages/Products'
 
 
-const routerDefinitions = createRoutesFromElements(
-  <Route>
-      <Route path='/' element={<Homepage />} />
-      <Route path='/products' element={<Products />} />
-  </Route>
-)
-// const router = createBrowserRouter([
-//   { path: '/', element: <Homepage /> },
-//   { path: '/products', element: <Products /> }
-// ])
-const router = createBrowserRouter(routerDefinitions)
+// const routerDefinitions = createRoutesFromElements(
+//   <Route>
+//       <Route path='/' element={<Homepage />} />
+//       <Route path='/products' element={<Products />} />
+//   </Route>
+// )
+// const router = createBrowserRouter(routerDefinitions)
+
+const router = createBrowserRouter([
+  { path: '/', element: <Homepage /> },
+  { path: '/products', element: <Products /> }
+])
 
 function App() {
   return <RouterProvider router={router} />;
