0119bdb626045c624597289fcb5d13b8935bd86a
German Bertea <gebe@influencerindex.com>
2023-02-06 20:36:12 +0100

    Create the routes with createRoutesFromElements

--- seccion20-ReactRouter/src/App.js
+++ seccion20-ReactRouter/src/App.js
@@ -1,12 +1,24 @@
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider
+} from 'react-router-dom'
 import Homepage from './pages/Home'
 import Products from './pages/Products'
 
 
-const router = createBrowserRouter([
-  { path: '/', element: <Homepage /> },
-  { path: '/products', element: <Products />  }
-])
+const routerDefinitions = createRoutesFromElements(
+  <Route>
+      <Route path='/' element={<Homepage />} />
+      <Route path='/products' element={<Products />} />
+  </Route>
+)
+// const router = createBrowserRouter([
+//   { path: '/', element: <Homepage /> },
+//   { path: '/products', element: <Products /> }
+// ])
+const router = createBrowserRouter(routerDefinitions)
 
 function App() {
   return <RouterProvider router={router} />;
