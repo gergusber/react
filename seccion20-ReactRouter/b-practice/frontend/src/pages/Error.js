3d73c584c25df4d46014f4a5429cce53dbd124a0
German Bertea <gebe@influencerindex.com>
2023-02-09 21:51:59 +0100

    error handling

--- seccion20-ReactRouter/b-practice/frontend/src/pages/Error.js
+++ seccion20-ReactRouter/b-practice/frontend/src/pages/Error.js
@@ -1,13 +1,29 @@
 import MainNavigation from '../components/MainNavigation';
+import PageContent from '../components/PageContent';
 import classes from './Error.module.css';
-
+import { useRouteError } from 'react-router-dom'
 
 const ErrorPage = () => {
+  const error = useRouteError();
+
+  let title = 'An error ocurred';
+  let message = 'something went wrong'
+
+  if (error.status === 500)
+    message = JSON.parse(error.data).message
+
+  if (error.status === 404) {
+    title = 'Not found'
+    message = 'Could not find resource or page'
+  }
+
+
   return <>
     <MainNavigation />
     <main >
-      <h1> Error ocurred</h1>
-      <p> could not find this page</p>
+      <PageContent title={title}>
+        <p> {message}</p>
+      </PageContent>
     </main>
   </>
 };
--- seccion20-ReactRouter/b-practice/frontend/src/pages/EventsPage.js
+++ seccion20-ReactRouter/b-practice/frontend/src/pages/EventsPage.js
@@ -5,6 +5,9 @@ const EventsPage = () => {
   const data = useLoaderData()
   const { events } = data;
 
+  if (data.isError) {
+    return <p>{data.message}</p>
+  }
   return <>
     <EventsList events={events} />
   </>
@@ -16,8 +19,10 @@ export default EventsPage;
 export async function loader() {
   const response = await fetch('http://localhost:8080/events');
 
-  if (!response.ok) { 
-    return { isError: true, message: 'Could not fetch events' }
+  if (!response.ok) {
+    throw new Response(
+      JSON.stringify({ message: 'Could not fetch events' }),
+      { status: 500 })
   } else {
     // const resData = await response.json();
     // setFetchedEvents(resData.events);
