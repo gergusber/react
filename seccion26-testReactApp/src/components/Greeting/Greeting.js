8576fe63441e4f8e0339e3e2ff78250d8803d030
German Bertea <gebe@influencerindex.com>
2023-02-25 14:29:28 +0100

    add arrange with other tests

--- seccion26-testReactApp/src/components/Greeting/Greeting.js
+++ seccion26-testReactApp/src/components/Greeting/Greeting.js
@@ -1,8 +1,21 @@
+import { useState } from "react"
+
+
 const Greeting = () => {
+
+  const [changeText, setChangedText] = useState(false)
+
+  const changeTextHandler = () => {
+    setChangedText(true)
+  }
+
   return (
     <div>
       <h2>HEllo world!</h2>
-      <p>Its a good to see you</p>
+      {!changeText && <p>Its a good to see you</p>}
+      {changeText && <p>Changed</p>}
+
+      <button onClick={changeTextHandler}> change text</button>
     </div>
   )
 }
--- seccion26-testReactApp/src/components/Greeting/Greeting.test.js
+++ seccion26-testReactApp/src/components/Greeting/Greeting.test.js
@@ -1,8 +1,12 @@
 import { render, screen } from "@testing-library/react"
 import Greeting from "./Greeting"
+import userEvents from "@testing-library/user-event"
+
 
 describe('Greeting component',
   () => {
+
+
 
     test('should have hello world as text', () => {
       // AAA
@@ -21,4 +25,58 @@ describe('Greeting component',
       expect(helloElement).toBeInTheDocument();
     })
 
+    test('should show the text button when not clicked', () => {
+      // AAA
+      //ARRANGE => SET UP TEST DATA
+      //ACT => RUN THE LOGIC.
+      //ASSERT => HAVE A LOOK OF THE OUTPUT
+
+      // ARRANGE
+      render(<Greeting />);
+
+
+      //ASSERT
+      const firstParagraphElement = screen.getByText('Its a good to see you', { exact: true });
+      expect(firstParagraphElement).toBeInTheDocument();
+    })
+
+
+    test('should show the text button when is clicked', () => {
+      // AAA
+      //ARRANGE => SET UP TEST DATA
+      //ACT => RUN THE LOGIC.
+      //ASSERT => HAVE A LOOK OF THE OUTPUT
+
+      // ARRANGE
+      render(<Greeting />);
+
+      //ACT
+      const buttonElement = screen.getByRole('button')
+      userEvents.click(buttonElement)
+
+
+      //ASSERT
+      const paragraphChangedElement = screen.getByText('Changed', { exact: true });
+      expect(paragraphChangedElement).toBeInTheDocument()
+    })
+
+
+    test('should not show the text of first paragraph button when is clicked', () => {
+      // AAA
+      //ARRANGE => SET UP TEST DATA
+      //ACT => RUN THE LOGIC.
+      //ASSERT => HAVE A LOOK OF THE OUTPUT
+
+      // ARRANGE
+      render(<Greeting />);
+
+      //ACT
+      const buttonElement = screen.getByRole('button')
+      userEvents.click(buttonElement)
+
+
+      //ASSERT
+      const paragraphChangedElement = screen.queryByText('Its a good to see you', { exact: true });
+      expect(paragraphChangedElement).toBeNull()
+    })
   })
