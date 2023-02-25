a1cbafccda08c950c69ce3aa9f59c28af50a6f2b
German Bertea <gebe@influencerindex.com>
2023-02-25 16:33:09 +0100

    Test with mock fetch data

--- seccion26-testReactApp/src/components/Async/Async.test.js
+++ seccion26-testReactApp/src/components/Async/Async.test.js
@@ -5,6 +5,18 @@ import Async from "./Async";
 
 describe('Async component', () => {
   test('render posts if request succeeds', async () => {
+    window.fetch = jest.fn();
+    window.fetch.mockResolvedValueOnce({
+      json: async () => [
+        {
+          id: 'p1', title: 'First post'
+        },
+        {
+          id: 'p2', title: 'Second post'
+        }
+      ]
+    });
+
     render(<Async />)
 
     const elements = await screen.findAllByRole('listitem', {}, {}) // list of roless https://www.w3.org/TR/html-aria/#docconformance
